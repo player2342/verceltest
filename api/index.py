d031bd3d8551219b1c22a6fd7488cffb2ded3ab9
mmgoumbala <mm.goumbala@adncorp.com>
2020-12-31 21:31:32 +0000

    and templates to serve the static file

--- api/index.py
+++ api/index.py
@@ -1,20 +1,29 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
+from flask import render_template
+
+from airtable import Airtable # import Airtable API python wrapper
+
+#Set airtable base infortion and API key
+base_key = 'apptAo2yky4AWLZfB' 
+table_name = 'emails'
+air_table_api_key = 'keymABXgPYp3BUmSL'
+airtable = Airtable(base_key, table_name, api_key=air_table_api_key)
+
 
 app = Flask(__name__)
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET'])    # define landing page route and serve the static file
 def welcome():
-    return "Welcome to my awesome API ! "
+    return render_template('index.html')
 
-@app.route('/api/', methods=['POST'])
+@app.route('/api/', methods=['POST']) # define the email capturing api route to /api/
 def capture_email():
-    email = request.form.get('email') # retrieve the email from the landing page
-    name = request.form.get('name')
-    cell = request.form.get('cell')
-    return jsonify({"email": email, "name": name, "cell": cell })
+    form_values = request.form.to_dict()    #retrieve form values
+    airtable.insert(form_values)            #insert values in airtable
+    return jsonify(request.form.to_dict())  
 
-@app.after_request
+@app.after_request                      # allow request to come from any origin
 def after_request(response):
     header = response.headers
     header['Access-Control-Allow-Origin'] = '*'
